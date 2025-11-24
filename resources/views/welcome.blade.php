9168c3ad54dbc70a3b9ecff6517402ac1dea1926
luisjose <luisjose@vboxlj.mired.com>
2025-11-24 10:57:52 +0100

    Actualización de la primera vista

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,35 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <title>Wheel Fire Club - Login</title>
-    <link rel="stylesheet" href="view1.css">
+    <link rel="stylesheet" href="{{asset('css/view1.css')}}">
 </head>
 <body>
 
 <div class="scene-container">
 
     <div id="mundo-real" class="world">
-        <div class="centered-button" id="open-gate-button">
-            <span class="stranger-things-text">LOGIN</span>
+        <div class="centered-title"> 
+            <span class="stranger-things-text">¿ERES NUEVO?</span>
         </div>
-        <div class="centered-button2" id="open-gate-button">
-            <span class="stranger-things-text">REGISTER</span>
+        <div class="centered-button" id="login-button">
+            <span class="stranger-things-text">INICIAR SESIÓN</span>
+        </div>
+        <div class="centered-button2" id="register-button"> 
+            <span class="stranger-things-text">REGISTRARSE</span>
         </div>
     </div>
         <div id="upside-down" class="world">
         <form action="{{ route('player.register') }}" method="POST">
             @csrf
             <input id="cuadrotexto" type="text" name="name" placeholder="Nombre" required>
-            <button id="botonjugar" type="submit">Jugar</button>
-        </form>
-        </div>
+            <button id="botonjugar" class="stranger-things-text" type="submit">JUGAR</button>
+        </form>  
+    </div>
 
-        <div id="crack"></div>
+    <div id="crack"></div>
+
 </div>
 
 <script>
-    document.getElementById('open-gate-button').addEventListener('click', function(){
+    // Código para el botón INICIAR SESIÓN (open-gate-button)
+    document.getElementById('login-button').addEventListener('click', function(){
         document.querySelector('.scene-container').classList.add('open');
-    })
+    });
+
+    // Código para el botón REGISTRARSE (register-button)
+    document.getElementById('register-button').addEventListener('click', function(){
+        document.querySelector('.scene-container').classList.add('open');
+    });
 </script>
 </body>
 </html>
